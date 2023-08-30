2fd246d6f73cbb87684755d85afbaa6e522e29f3
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-08-30 19:47:01 +0530

    Update Member.java

--- AP_Assignment1/src/main/java/org/example/Member.java
+++ AP_Assignment1/src/main/java/org/example/Member.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import javax.swing.text.html.parser.TagElement;
+
 public class Member {
     private String name;
-    private int age;
+    private float age;
     private int n_books;
+    private int phone_no;
+    private int ID;
+    private boolean isRemoved;
+
+    public Member(String name, float age, int phone_no, int ID){
+        this.name = name;
+        this.age = age;
+        this.phone_no = phone_no;
+        this.ID = ID;
+        this.isRemoved = false;
+        this.n_books = 0;
+    }
+
+    public int getPhone_no(){
+        return this.phone_no;
+    }
+
+    public int getID(){
+        return this.ID;
+    }
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
+    public void setRemovedStatus(boolean value){
+        this.isRemoved = value;
+    }
 }
