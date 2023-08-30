1e52e827a9c39ce96bc87295c4a3a4760bc15c19
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-08-30 10:54:06 +0530

    Update Book.java

--- AP_Assignment1/src/main/java/org/example/Book.java
+++ AP_Assignment1/src/main/java/org/example/Book.java
@@ -1,10 +1,45 @@
 package org.example;
 
 public class Book {
-    private String ID;
+    private int ID;
     private String title;
     private String author;
-    private int total_copies;
+    private boolean isBorrowed;
+    private boolean isDeleted;
 
+    public Book(String title, String author, int ID){
+        this.title = title;
+        this.author = author;
+        this.ID = ID;
+        this.isBorrowed = false;
+        this.isDeleted = false;
+    }
 
+    public int getID(){
+        return this.ID;
+    }
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public boolean getBorrowedStatus(){
+        return this.isBorrowed;
+    }
+
+    public boolean getDeletedStatus(){
+        return this.isDeleted;
+    }
+
+    public void setDeletedStatus(boolean value){
+        this.isDeleted = value;
+    }
 }
